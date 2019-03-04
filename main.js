5372f8ea023f9274e37c9388e7295b01216361c9
unknown <felix.robaglia@gmail.com>
2019-03-04 12:00:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let CurrentKeyCode = document.querySelector('h1');
+let CurrentKey = document.querySelector('h2');
+
+document.addEventListener('keydown', function(event) {
+  CurrentKeyCode.innerHTML = event.keyCode;
+  CurrentKey.innerHTML = event.key;
+})
